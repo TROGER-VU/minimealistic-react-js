6d8e4d00f683c99241cfa8018399a5561244bc82
TROGER-VU <ayushgupta91331@gmail.com>
2024-01-09 00:56:45 +0530

    Login Page

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import foodRouter from './routers/food.router.js';
+import userRouter from './routers/user.router.js';
 const app = express();
-
+app.use(express.json());
 app.use(cors({
     credentials:true,
     origin: ['http://localhost:3000'],
@@ -10,6 +11,7 @@ app.use(cors({
 );
 
 app.use('/api/foods', foodRouter);
+app.use('/api/users', userRouter);
 
 const PORT = 5000;
 app.listen(PORT, () => {
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './header.module.css';
 import { useCart } from '../../hooks/useCart';
+import { useAuth } from '../../hooks/useAuth';
 export default function Header() {
-    const user = {
-        name: 'John', 
-    };
+    const { user, logout} = useAuth();
 
-    const logout = () => {}
 
     const {cart} = useCart();
 
